e117f6337a79997d216e72650f131bff45e40f8f
Vanessa Rocha <Vanessa.Rocha@fit-tecnologia.org.br>
2024-04-12 21:17:40 -0400

    add home and sign

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { HotelComponent } from './hotel/hotel.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
 import { appComponentRoutes } from './app.component.routes';
+import { AppComponent } from './app.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HotelComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
